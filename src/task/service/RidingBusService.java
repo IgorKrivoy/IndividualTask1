253add1532044593117fd5c0d962734ae2cf1d0e
IgorKrivoy <darckboom00@gmai.com>
2019-12-03 12:03:53 +0200

    Fixed threads

--- src/task/service/RidingBusService.java
+++ src/task/service/RidingBusService.java
@@ -9,22 +9,26 @@ public class RidingBusService extends Service implements Runnable {
     @Override
     public void run() {
         for (TransportStopModel transportStopModel : transportStopList) {
+            lock.lock();
             numberNowStop = transportStopModel.getNumber();
             outPut.busComeToStop(numberNowStop);
             outPut.tenSecondToGo();
             Thread myThread2 = new Thread(takeAndOutPassengerService);
             myThread2.start();
+
             try {
-                Thread.sleep(10000);
+                myThread2.join();
+                Thread.sleep(7000);
                 if(numberNowStop !=5) {
                     outPut.busGoingToNextStop();
                 } else {
                     outPut.busGoingToAutoPark();
                 }
-                Thread.sleep(5000);
+                Thread.sleep(4000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            lock.unlock();
         }
 
     }
